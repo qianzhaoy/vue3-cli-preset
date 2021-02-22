51cad36bf3d02667e4e04337037e1d2206d1b906
钱招严 <devqianzy@gmail.com>
2021-02-22 23:26:34 +0800

    update

--- generator/index.js
+++ generator/index.js
@@ -1,6 +1,17 @@
+const elementPlus = require('../plugins/vue-cli-plugin-element-plus/generator')
+const { ELEMENT_PLUS, VANT } = require('../constant')
 module.exports = function(api, opts, rootOptions) {
   api.render({
     './src/constants/enums.ts': './template/src/constants/enums.ts',
     './src/constants/config.ts': './template/src/constants/config.ts',
   })
+  if (opts.ui_lib)
+
+  switch (opts.ui_lib) {
+    case ELEMENT_PLUS:
+      elementPlus(api, opts, rootOptions)
+      break;
+    case VANT:
+      break;
+  }
 }
